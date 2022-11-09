ed59fddd1371bec37797761dddf4c0fb08e3b4b3
Sergey Smirnov <condorgerg@gmail.com>
2022-11-09 18:06:27 +0500

    Module 5 t01

---
+++ src/Module5/t01/Main.java
@@ -0,0 +1,15 @@
+package Module5.t01;
+
+/**
+ * Используя StringBuilder напишите любое одностишие
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Делу время,");
+        sb.append(" а потехе час.");
+        System.out.println(sb);
+
+
+    }
+}
